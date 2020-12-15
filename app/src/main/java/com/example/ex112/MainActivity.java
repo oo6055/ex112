b86cfd837f3f5e3a5ee44d3617d52bcd8d2a1600
OriOfek <oriofek106@gmail.com>
2020-12-15 15:28:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex112/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ex112;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et;
+    TextView tv;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.editText);
+        tv = (TextView) findViewById(R.id.textView);
+        
+        getData();
+    }
+
+    private void getData() {
+        SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
+        String str = settings.getString("str","");
+        counter = settings.getInt("counter",0);
+        et.setText(str);
+        tv.setText(counter);
+    }
+
+    public void count(View view) {
+        counter++;
+        tv.setText(String.valueOf(counter));
+    }
+
+    public void reset(View view) {
+        counter = 0;
+        tv.setText(String.valueOf(counter));
+    }
+
+    public void exit(View view) {
+        SharedPreferences.Editor editor = getSharedPreferences("PREFS_NAME", MODE_PRIVATE).edit();
+        editor.putString("str", et.getText().toString());
+        editor.putInt("counter",counter);
+        editor.commit();
+        finish();
+    }
+}
